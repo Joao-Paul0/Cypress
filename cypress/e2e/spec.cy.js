94405c51b547eeceb32650b4e444bea724933730
Joao-Paul0 <joaopaulo.tomazaquino@hotmail.com>
2024-07-16 19:48:31 -0300

    Exercícios finalizados

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -10,4 +10,28 @@ describe('Página de cadastro', () => {
     cy.get('input[name="confirm_password"]').type('Qwe123');
     cy.contains('button', 'Cadastrar').click();
   })
+});
+
+describe('Visite a página de principal do AdoPet e clique no botão "Ver pets disponíveis para adoção”', () => {
+  it('Ver pets disponíveis para adoção', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.contains('a', 'Ver pets disponíveis para adoção').click();
+  })
+});
+
+describe('Visite a página de principal do AdoPet e teste os botões header', () => {
+  it('Clicar nos botões header', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.get('a[aria-label="Tela inicial"]').click();
+    cy.get('a[aria-label="Ir para Mensagens"]').click();
+  })
+});
+
+describe('Visite página de login do Adopet', () => {
+  it('Deve carregar a página de login', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/login');
+    cy.get('input[name="email"]').type('jao@email.com');
+    cy.get('input[name="password"]').type('Qwe123');
+    cy.contains('button', 'Entrar').click();
+  })
 })
